a7ea39de80d9100eac7c9a89f9f6a1496a1e5a4b
Danny Phillips <danielphillips0101@gmail.com>
2022-01-08 01:41:13 -0800

    Added local storage of text after save press.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,9 @@
 var timeAreaElement = document.getElementsByName('textarea')
 var timeText = document.getElementsByClassName('timeText')
+var saveBtn = document.getElementsByClassName('saveBtn')
 var hourTime = parseInt(moment().format('H'))
 
+//displays current time at top of page
 function displayCurrentTime() {
     setTimeout(displayCurrentTime, 1000);
     var exactTime = moment()
@@ -9,6 +11,7 @@ function displayCurrentTime() {
 }
 displayCurrentTime();
 
+//Color change based on calendar hour vs current hour
 Array.from(timeText).forEach(textarea => {
     var textAreaIDString = textarea.id;
     var textAreaHour;
@@ -25,10 +28,39 @@ Array.from(timeText).forEach(textarea => {
         if (hourTime > textAreaHour) {
             textarea.classList.add('past')
         }
-    } 
+    }
+    
 })
 
+//Locally stores date entered into text fields upon click of save button
+Array.from(saveBtn).forEach(button => {
+        button.addEventListener('click', function() {
+            var key = button.name
+            var calendarEvent = $(button).siblings('.description')[0]
+            
+            localStorage.setItem(key, calendarEvent.value)
+            console.log(localStorage.getItem(key))
+        })
+    })
 
 
+
+// document.get
+// localStorage.setItem('9AM', document.getElementsByName('nineText').innerText)
+
+
+// localStorage.setItem('10AM', document.getElementsByName('tenText').innerText)
+// localStorage.setItem('11AM', document.getElementsByName('elevenText').innerText)
+// localStorage.setItem('12PM', document.getElementsByName('twelveText').innerText)
+// localStorage.setItem('1PM', document.getElementsByName('oneText').innerText)
+// localStorage.setItem('2PM', document.getElementsByName('twoText').innerText)
+// localStorage.setItem('3PM', document.getElementsByName('threeText').innerText)
+// localStorage.setItem('4PM', document.getElementsByName('fourText').innerText)
+// localStorage.setItem('5PM', document.getElementsByName('fiveText').innerText)
+
+// function saveCalendarData () {
+//     var calendarData= document.getElementsByClassName('description').value;
+//     localStorage.setItem('')
+// }
 
 
